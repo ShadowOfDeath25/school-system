5aa81f6bc5c1cf0c887ec133b171cb8a20d8e2ec
Abdelrahman Rafeek <152920007+ShadowOfDeath25@users.noreply.github.com>
2025-09-06 20:25:08 +0300

    Restructured the database table structure to make the relation between the student and his/her guardian a many-to-many relationship

---
+++ database/migrations/2025_09_06_162009_create_guardian_student_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guardian_student', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('guardian_id')->unsigned();
+            $table->foreign('guardian_id')->references('id')->on('guardians');
+            $table->bigInteger("student_id")->unsigned();
+            $table->foreign("student_id")->references("id")->on("students");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guardian_student');
+    }
+};
