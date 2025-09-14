3a374ceca8721b5e78c88780b307715ecf699465
Abdelrahman Rafeek <152920007+ShadowOfDeath25@users.noreply.github.com>
2025-09-14 03:54:24 +0300

    made the hover effect on the menu Icon more noticable

--- frontend/src/components/ui/Navbar/Navbar.jsx
+++ frontend/src/components/ui/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 import styles from './styles.module.css'
 import {IconButton} from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
-import {useState} from "react";
+
 
 export default function Navbar({handleMenuClick}) {
 
 
     return (
         <nav className={styles.navbar}>
-            <IconButton onClick={handleMenuClick}><MenuIcon className={styles.menuIcon}/></IconButton>
+            <IconButton sx={{"&:hover":{backgroundColor:"rgba(0,0,0,0.2)"}}} onClick={handleMenuClick}><MenuIcon className={styles.menuIcon}/></IconButton>
             <img src="/public/logo.svg" alt="logo"/>
             <div className="profile">Profile</div>
         </nav>
