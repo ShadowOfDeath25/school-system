3318516e7f81c39b90c1aabd4ab7ce36d82141bd
Abdelrahman Rafeek <152920007+ShadowOfDeath25@users.noreply.github.com>
2025-09-21 06:44:46 +0300

    added redirection for unauthenticated users back to the main page of the client side

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-Route::get('/', function () {
-    return view('welcome');
-});
-// guards/web.php
-Route::get('/debug-session', function (Request $request) {
-    return response()->json([
-        'session_id' => session()->getId(),
-        'session_data' => session()->all(),
-        'is_authenticated' => auth()->check(),
-        'user' => auth()->user(),
-        'cookies' => $request->cookies->all(),
-    ]);
-});
