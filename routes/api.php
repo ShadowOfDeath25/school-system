470fa650ad4706f9fced03f88baf296a3e963f59
Abdelrahman Rafeek <152920007+ShadowOfDeath25@users.noreply.github.com>
2025-09-23 22:20:46 +0300

    made sure that all the routes are properly protected.

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,22 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BankAccountController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\BuildingController;
 use App\Http\Controllers\BusController;
 use App\Http\Controllers\ClassroomController;
+use App\Http\Controllers\ExamController;
+use App\Http\Controllers\ExamHallController;
 use App\Http\Controllers\ExpensesController;
+use App\Http\Controllers\FloorController;
 use App\Http\Controllers\GuardianController;
 use App\Http\Controllers\IncomeController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\SeatNumberController;
+use App\Http\Controllers\SecretNumberController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\UserController;
@@ -35,6 +42,14 @@ Route::middleware("auth:sanctum")->group(function () {
     Route::apiResource('expenses', ExpensesController::class)->withFilters();
     Route::apiResource('roles', RoleController::class)->withFilters();
     Route::apiResource('users', UserController::class)->withFilters();
+    Route::apiResource('buildings', BuildingController::class);
+    Route::apiResource('floors', FloorController::class);
+    Route::apiResource('exam-halls', ExamHallController::class);
+    Route::apiResource('bank-accounts', BankAccountController::class);
+    Route::apiResource('secret-numbers', SecretNumberController::class);
+    Route::apiResource('exams', ExamController::class);
+    Route::apiResource('seat-numbers', SeatNumberController::class);
+
 
     Route::patch('/users/{user}/roles', UserController::class . "@assignRole")->name("users.roles.assign");
     Route::put('/users/{user}/roles', UserController::class . "@syncRole")->name("users.roles.sync");
@@ -42,32 +57,3 @@ Route::middleware("auth:sanctum")->group(function () {
 
     Route::get("/permissions", [PermissionController::class, 'index']);
 });
-//=======
-Route::apiResource("books", BookController::class)->withFilters();
-Route::apiResource('buses', BusController::class);
-Route::apiResource('parents', GuardianController::class);
-Route::apiResource('students', StudentController::class)->withFilters();
-Route::apiResource('payments', PaymentController::class);
-Route::apiResource('incomes', IncomeController::class);
-Route::apiResource('classrooms', ClassroomController::class);
-Route::apiResource('subjects', SubjectController::class);
-Route::apiResource('expenses', ExpensesController::class);
-Route::apiResource('roles', RoleController::class);
-Route::apiResource('users', UserController::class)->withFilters();
-Route::apiResource('buildings', BuildingController::class);
-Route::apiResource('floors', FloorController::class);
-Route::apiResource('exam-halls', ExamHallController::class);
-Route::apiResource('bank-accounts', BankAccountController::class);
-Route::apiResource('secret-numbers', SecretNumberController::class);
-Route::apiResource('exams', ExamController::class);
-Route::apiResource('seat-numbers', SeatNumberController::class);
-Route::patch('/users/{user}/roles', UserController::class . "@assignRole")->name("users.roles.assign");
-Route::put('/users/{user}/roles', UserController::class . "@syncRole")->name("users.roles.sync");
-Route::delete("/users/{user}/roles", UserController::class . "@removeRole")->name("users.roles.remove");
-
-Route::get("/permissions",[PermissionController::class ,'index']);
-
-//<<<<<<< Updated upstream
-//>>>>>>> Stashed changes
-//=======
-//>>>>>>> Stashed changes
