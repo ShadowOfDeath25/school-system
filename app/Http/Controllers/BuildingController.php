772da9d75e2b09102a2f067458dd832113874d8c
Abdelrahman Rafeek <152920007+ShadowOfDeath25@users.noreply.github.com>
2025-09-25 10:53:42 +0300

    Added missing imports

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Building\StoreBuildingRequest;
+use App\Http\Requests\Building\UpdateBuildingRequest;
 use App\Models\Building;
+use App\Traits\HasCRUD;
+use App\Traits\HasFilters;
 use Illuminate\Http\Request;
 
 class BuildingController extends Controller
 {
-    
+
     use HasCRUD,HasFilters;
     protected string $model = Building::class;
     protected string $storeRequest = StoreBuildingRequest::class;
--- app/Http/Controllers/ExamHallController.php
+++ app/Http/Controllers/ExamHallController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ExamHall\StoreExamHallRequest;
+use App\Http\Requests\ExamHall\UpdateExamHallRequest;
 use App\Models\ExamHall;
 use Illuminate\Http\Request;
 use App\Traits\HasCRUD;
@@ -14,7 +15,7 @@ class ExamHallController extends Controller
    use HasCRUD;
     protected string $model = ExamHall::class;
     protected string $storeRequest = StoreExamHallRequest::class;
-    protected string $updateRequest = \App\Http\Requests\ExamHall\UpdateExamHallRequest::class;
+    protected string $updateRequest = UpdateExamHallRequest::class;
 
 
 }
