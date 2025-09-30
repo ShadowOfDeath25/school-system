d4b6da2e547114b1992bda3fb0edf51d662fa309
Abdelrahman Rafeek <152920007+ShadowOfDeath25@users.noreply.github.com>
2025-09-30 11:36:27 +0300

    added missing imports

--- app/Http/Controllers/BankAccountController.php
+++ app/Http/Controllers/BankAccountController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BankAccount\StoreBankAccountRequest;
+use App\Http\Requests\BankAccount\UpdateBankAccountRequest;
 use App\Models\BankAccount;
 use Illuminate\Http\Request;
 use \App\Traits\HasCRUD;
@@ -10,6 +12,6 @@ class BankAccountController extends Controller
 {
     use hasCRUD;
     protected string $model = BankAccount::class;
-    protected string $storeRequest = \App\Http\Requests\BankAccount\StoreBankAccountRequest::class;
-    protected string $updateRequest = \App\Http\Requests\BankAccount\UpdateBankAccountRequest::class;
+    protected string $storeRequest = StoreBankAccountRequest::class;
+    protected string $updateRequest = UpdateBankAccountRequest::class;
 }
