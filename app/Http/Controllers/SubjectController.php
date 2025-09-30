d1982f6d058a0ae04bcdc6060ed3de815dabc0e9
Abdelrahman Rafeek <152920007+ShadowOfDeath25@users.noreply.github.com>
2025-10-01 01:42:41 +0300

    Added the option to handle the types query parameter

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -8,10 +8,14 @@ use App\Http\Resources\SubjectResource;
 use App\Models\subject;
 use App\Traits\HasCRUD;
 use App\Traits\HasFilters;
+use Illuminate\Http\Request;
 
 class SubjectController extends Controller
 {
-    use HasCRUD,HasFilters;
+    use HasCRUD {
+        HasCRUD::index as baseIndex;
+    }
+    use HasFilters;
 
     protected array $filterable = [
         'academic_year',
@@ -25,4 +29,20 @@ class SubjectController extends Controller
     protected string $storeRequest = StoreSubjectRequest::class;
     protected string $updateRequest = UpdateSubjectRequest::class;
     protected string $resource = SubjectResource::class;
+
+    public function index(Request $request)
+    {
+        if ($request->has('types')) {
+            return SubjectResource::collection(
+                $this->getQuery()
+                    ->distinct('type')->
+                    paginate($request->
+                    input('per_page', 30))
+                    ->withQueryString());
+        } else {
+            return $this->baseIndex($request);
+        }
+    }
+
+
 }
