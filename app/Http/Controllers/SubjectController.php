f990bd27c661de9fffc03c5ce3ce4a102ddd23f9
Abdelrahman Rafeek <152920007+ShadowOfDeath25@users.noreply.github.com>
2025-09-30 23:56:37 +0300

    Added the filters for the subjects.jsx

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -5,11 +5,20 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Subject\StoreSubjectRequest;
 use App\Http\Requests\Subject\UpdateSubjectRequest;
 use App\Models\subject;
-use Illuminate\Http\Request;
 use App\Traits\HasCRUD;
+
 class SubjectController extends Controller
 {
     use HasCRUD;
+
+    protected array $filterable = [
+        'a  cademic_year',
+        'language',
+        'level',
+        'grade',
+        'semester',
+        'type'
+    ];
     protected string $model = subject::class;
     protected string $storeRequest = StoreSubjectRequest::class;
     protected string $updateRequest = UpdateSubjectRequest::class;
