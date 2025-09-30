b120e7743ac11caa03c87fc4267f1cc168001a4d
Abdelrahman Rafeek <152920007+ShadowOfDeath25@users.noreply.github.com>
2025-09-30 23:45:01 +0300

    added this helper instead of rewriting it's logic every single time

---
+++ frontend/src/utils/getGradeOptionsByLevel.js
@@ -0,0 +1,22 @@
+export const getGradeOptionsByLevel = (FormData) => {
+    const gradeOptionsByLevel = {
+        "رياض اطفال": [
+            {label: "الأول", value: 1},
+            {label: "الثاني", value: 2},
+        ],
+        "ابتدائي": [
+            {label: "الاول", value: 1},
+            {label: "الثاني", value: 2},
+            {label: "الثالث", value: 3},
+            {label: "الرابع", value: 4},
+            {label: "الخامس", value: 5},
+            {label: "السادس", value: 6},
+        ],
+        "اعدادي": [
+            {label: "الأول", value: 1},
+            {label: "الثاني", value: 2},
+            {label: "الثالث", value: 3},
+        ],
+    };
+    return gradeOptionsByLevel[FormData.level];
+}
