9c0c36c1813d45c75e5cd0539c9f5f8081d54b5e
Abdelrahman Rafeek <152920007+ShadowOfDeath25@users.noreply.github.com>
2025-10-29 01:46:43 +0300

    feat: add tab navigation for payment types in PaymentValues component and update styles

--- frontend/src/components/pages/Payments/PaymentValues.jsx
+++ frontend/src/components/pages/Payments/PaymentValues.jsx
@@ -2,11 +2,44 @@ import styles from './styles.module.css'
 import Page from "@ui/Page/Page.jsx";
 import SelectField from "@ui/SelectField/SelectField.jsx";
 import {ClassroomHelper} from "@helpers/ClassroomHelper.js";
+import {Tab, Tabs} from "@mui/material";
+import {useState} from "react";
 
+const CustomTab = (props) => {
+    return (
+        <Tab
+            {...props}
+            sx={{
+                color: "white",
+                '&.Mui-selected': {
+                    color: "var(--primary-text-color)",
+                    backgroundColor: "rgba(0,0,0,.2) !important"
+                }
+            }}
+        />
+    )
+}
 export default function PaymentValues() {
+    const [tab, setTab] = useState();
     return (
         <Page>
             <form className={styles.container}>
+                <div className={styles.tabsWrapper}>
+                    <span>انواع المدفوعات</span>
+                    <Tabs
+                        value={tab}
+                        onChange={(e, newValue) => setTab(newValue)}
+                        sx={{
+                            "& .MuiTabs-indicator":{
+                                backgroundColor: "var(--secondary-color)"
+                            }
+                        }}
+                    >
+                        <CustomTab id={"1"} label={'tab1'}/>
+                        <CustomTab id={"2"} label={'tab1'}/>
+                        <CustomTab id={"3"} label={'tab1'}/>
+                    </Tabs>
+                </div>
                 <SelectField
                     {...ClassroomHelper.FIELDS.ACADEMIC_YEAR}
                 />
