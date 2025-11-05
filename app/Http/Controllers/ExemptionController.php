9b27af66be0b903fbb986cfc3127eb6c6b74c318
Abdelrahman Rafeek <152920007+ShadowOfDeath25@users.noreply.github.com>
2025-11-05 23:17:22 +0200

    fix: correct Arabic translations and improve code formatting in multiple files

---
+++ app/Http/Controllers/ExemptionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Exemption\StoreExemptionRequest;
+use App\Http\Requests\Exemption\UpdateExemptionRequest;
+use App\Models\Exemption;
+use App\Traits\HasCRUD;
+
+class ExemptionController extends Controller
+{
+    use HasCRUD;
+
+    protected string $model = Exemption::class;
+    protected string $storeRequest = StoreExemptionRequest::class;
+    protected string $updateRequest = UpdateExemptionRequest::class;
+}
---
+++ database/migrations/2025_11_03_230641_create_exemptions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Student;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exemptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Student::class)->constrained()->cascadeOnDelete();
+            $table->decimal("value", 10, 2);
+            $table->string("type");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exemptions');
+    }
+};
