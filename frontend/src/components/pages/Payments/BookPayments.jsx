d2dafbbcc17644970ac9f7b6adf4c42ac5ebf200
Abdelrahman Rafeek <152920007+ShadowOfDeath25@users.noreply.github.com>
2025-12-04 00:02:37 +0200

    feat: add academicYear prop to BookPicker component in BookPayments

--- frontend/src/components/pages/Payments/BookPayments.jsx
+++ frontend/src/components/pages/Payments/BookPayments.jsx
@@ -56,6 +56,7 @@ export default function BookPayments() {
             />
             <BookPicker
                 student={student}
+                academicYear={academicYear}
             />
         </DetailsPage>
     );
