6985a43f324b76a8696949aa20507b3e6a4dc86c
Abdelrahman Rafeek <152920007+ShadowOfDeath25@users.noreply.github.com>
2025-12-14 02:05:07 +0200

    feat: Integrate ItemPicker component into UniformPayments for enhanced uniform management

--- frontend/src/components/pages/Payments/UniformPayments.jsx
+++ frontend/src/components/pages/Payments/UniformPayments.jsx
@@ -1,7 +1,7 @@
 import PaymentsTable from "@ui/PaymentsTable/PaymentsTable.jsx";
 import {useOutletContext} from "react-router";
 import {PaymentHelper} from "@helpers/PaymentHelper.js";
-import UniformPicker from "@ui/UniformPicker/UniformPicker.jsx";
+import ItemPicker from "@ui/ItemPicker/ItemPicker.jsx";
 
 export default function UniformPayments() {
     const {student, academicYear} = useOutletContext()
@@ -13,9 +13,31 @@ export default function UniformPayments() {
                 btnText={"اضافة حركة مصروفات الزي"}
                 type={PaymentHelper.PAYMENT_TYPES.UNIFORM}
             />
-            <UniformPicker
+            <ItemPicker
                 student={student}
                 academicYear={academicYear}
+                config={{
+                    resourceName: 'uniform',
+                    purchaseEndpoint: 'uniform-purchases',
+                    buttonText: "اضافة زي",
+                    columns: [
+                        {key: "type", label: "النسخة"},
+                        {key: "quantity", label: "الكمية"},
+                        {key: "size", label: "المقاس"},
+                        {key: "price", label: "السعر", render: (purchase) => purchase.price - 0},
+                        {key: "total_price", label: "الإجمالي", render: (purchase) => purchase.total_price - 0},
+                    ],
+                    itemParams: {
+                        student_id: student.id,
+                        academic_year: academicYear,
+                        level: student.classroom.level
+                    },
+                    purchaseParams: {
+                        student_id: student.id,
+                        academic_year: academicYear
+                    },
+
+                }}
             />
         </>
     );
