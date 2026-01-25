393b4ef291e1e87001786a33a79f94c03f13ee19
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-01-26 00:46:25 +0200

    feat: add building and floor selection to AddClassrooms form

--- frontend/src/components/pages/Classrooms/AddClassrooms.jsx
+++ frontend/src/components/pages/Classrooms/AddClassrooms.jsx
@@ -1,18 +1,17 @@
 import Page from "@ui/Page/Page.jsx";
 import Form from "@ui/Form/Form.jsx";
-import { useCreate, useGetAll } from "@hooks/api/useCrud.js";
-import { useSnackbar } from "@contexts/SnackbarContext.jsx";
-import { useState } from "react";
-import { ClassroomHelper } from "@utils/helpers/ClassroomHelper.js";
-
+import {useCreate, useGetAll} from "@hooks/api/useCrud.js";
+import {useSnackbar} from "@contexts/SnackbarContext.jsx";
+import {useState} from "react";
+import {ClassroomHelper} from "@utils/helpers/ClassroomHelper.js";
 
 
 export default function AddClassrooms() {
     const creationMutation = useCreate("classrooms");
-    const { data: academicYears = [] } = useGetAll('academic-years');
-    const { showSnackbar } = useSnackbar();
+    const {data: academicYears = []} = useGetAll('academic-years');
+    const {data: buildings = []} = useGetAll('buildings');
+    const {showSnackbar} = useSnackbar();
     const [serverErrors, setServerErrors] = useState(null);
-
 
 
     const onFormSubmit = (data, formActions) => {
@@ -35,7 +34,36 @@ export default function AddClassrooms() {
     return (
         <Page>
             <Form
-                fields={ClassroomHelper.getAllFields().filter(field => field.name !== 'classroom').map(field => field.name === 'academic_year' ? { ...field, options: academicYears } : field)}
+                fields={
+                    [
+                        ...ClassroomHelper.getAllFields().filter(field => field.name !== 'classroom')
+                            .map(field => field.name === 'academic_year' ? {...field, options: academicYears} : field),
+                        {
+                            name: "building",
+                            type: "select",
+                            options: buildings?.data?.map(building => ({
+                                label: building.name,
+                                value: buildings.data.indexOf(building)
+                            })),
+                            required: true,
+                            label: "المبني",
+                            placeholder: "اختر المبني"
+                        },
+                        {
+                            name: "floor_id",
+                            type: "select",
+                            options: (buildingIndex) => buildings?.data?.[buildingIndex]?.floors?.map(floor => ({
+                                label: floor.name,
+                                value: floor.id
+                            })),
+                            dependency: "building",
+                            required: true,
+                            label: "الطابق",
+                            placeholder: "اختر الطابق",
+                            disabled: (buildingIndex) => (buildingIndex ==="")
+                        }
+
+                    ]}
                 onFormSubmit={onFormSubmit}
                 serverErrors={serverErrors}
             />
