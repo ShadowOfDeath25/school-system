fb843390b4f1fa625137d8776cace38c66edba38
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-01-27 01:43:58 +0200

    added the pdf generation logic (untested)

--- app/Http/Controllers/StudentReportsController.php
+++ app/Http/Controllers/StudentReportsController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use App\Enums\PaymentType;
 use App\Http\Requests\StudentReports\StudentLetterRequest;
 use App\Services\StudentReportsService;
+use Illuminate\Http\Request;
+use Spatie\LaravelPdf\Facades\Pdf;
+use Storage;
+use Str;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class StudentReportsController extends Controller
@@ -45,11 +49,27 @@ class StudentReportsController extends Controller
      */
     public function studentLetters(StudentLetterRequest $request, StudentReportsService $service): JsonResponse
     {
-        $classrooms = $service->getStudentsGroupedByClassrooms(
+        $uuid = Str::uuid()->toString();
+        $filePath = "reports/$uuid.pdf";
+
+        $studentsByClassrooms = $service->getStudentsGroupedByClassrooms(
             ...$this->extractStudentFilters($request)
         );
+        $pdf = Pdf::view("reports.layouts.base",
+            [
+                "letter" => $request->validated('letter'),
+                $studentsByClassrooms
+            ]
+        );
 
-        return response()->json($classrooms);
+        Storage::put($filePath, $pdf);
+
+        return response()->json([
+            'uuid' => $uuid,
+            'preview_url' => route("reports.preview", $uuid)
+        ]);
+
+
     }
 
     /**
@@ -66,11 +86,27 @@ class StudentReportsController extends Controller
             'academicYear' => $validated['academic_year'] ?? null,
             'language' => $validated['language'] ?? null,
             'level' => $validated['level'] ?? null,
-            'grade' => isset($validated['grade']) ? (int) $validated['grade'] : null,
-            'classroom' => isset($validated['classroom']) ? (int) $validated['classroom'] : null,
-            'min' => isset($validated['min']) ? (float) $validated['min'] : null,
+            'grade' => isset($validated['grade']) ? (int)$validated['grade'] : null,
+            'classroom' => isset($validated['classroom']) ? (int)$validated['classroom'] : null,
+            'min' => isset($validated['min']) ? (float)$validated['min'] : null,
             'sorting' => $validated['sorting'] ?? null,
             'type' => $validated['type'] ?? null,
         ];
     }
+
+    public function preview(string $uuid)
+    {
+        $file_path = storage_path("app/reports/$uuid.pdf");
+
+        if (!file_exists($file_path)) {
+            abort(404, "هذا التقرير غير موجود");
+        }
+
+        return response()->file($file_path, [
+            'Content-Type' => "application/pdf",
+            'Content-Disposition' => 'inline'
+        ])->deleteFileAfterSend(true);
+    }
+
+
 }
