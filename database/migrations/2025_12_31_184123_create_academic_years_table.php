fe418256fd6f4ffb3169817167a4bdaea49b995b
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-03-04 05:14:16 +0200

    fix: fixed a bug in the academic_years table where the active flag had no default value

--- database/migrations/2025_12_31_184123_create_academic_years_table.php
+++ database/migrations/2025_12_31_184123_create_academic_years_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('academic_years', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->boolean('active');
+            $table->boolean('active')->default(false);
             $table->timestamps();
         });
     }
